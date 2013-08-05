8e2151e11bf29cd33ef21e4d3dce7abf76ec0b8e
Vincent Gijsen <vincent@sciencerockstars.com>
2013-08-06 00:28:49 +0200

    fixes

--- _433Bridge.ino
+++ _433Bridge.ino
@@ -1,10 +1,16 @@
 #include <RemoteReceiver.h>
 #include <RemoteTransmitter.h>
+//#include <VirtualWire.h>
+
+#include <SpaceLen.h>
+
 
 #define BLINKDELAY 5
 #define SYSTEMCODE 31
 #define TRANSMITTER P1_5
 #define RECEIVER P1_4
+ 
+const char *HELP = "action:  a|0..31 (char)sys|A-E (charCap)dev|0-1(on/of)\n blokker: b|1-8 (char)|0-1 onof(char)\n Elro:    e|0..31(char)|A-D CAP||0-1(on/of)\n KAKU:    k|A-P (char)add|1..4(char)group|1..4 (char)dev) |0-1 onof|\n Test: t\\r\n  eg: a31B1 -> put 31 as char 31 (one byte)\0";
 
 ElroTransmitter elroTransmitter(TRANSMITTER);
 BlokkerTransmitter blokkerTransmitter(TRANSMITTER);
@@ -15,9 +21,20 @@ String inputString = "";
 boolean stringComplete = false;
 
 
+
 void p(String input){
   Serial.println(input);
-}
+ }
+
+//SpaceLen stuff
+
+volatile uint8_t data[4]= {0,0,0,0};
+volatile boolean SENSORFLAG = false;
+
+//int count =0;
+volatile long lastCode = 0;
+volatile long lastPeriod = 0;
+volatile bool REMOTEFLAG=false;
 
 
 
@@ -26,6 +43,9 @@ void setup()
   Serial.begin(9600);
   Serial.println("started");
   inputString.reserve(20);
+  
+  //Serial.println(*HELP);
+  
   // put your setup code here, to run once:M
    pinMode(RECEIVER, INPUT);
    
@@ -37,40 +57,137 @@ void setup()
    pinMode(RED_LED, OUTPUT);
    digitalWrite(RED_LED, LOW);
  
-   blink();  
+   blink(); 
+   
+   //Attach interrupts
+  SpaceLen::init(RECEIVER, handleSensorData);
+  attachInterrupt(RECEIVER, interruptWrapper, RISING); 
+  
+  //Virtual Wire Settings
+  // vw_set_ptt_inverted(true); // Required for DR3100
+  /*
+   vw_set_ptt_pin(P2_1);
+   vw_set_rx_pin(RECEIVER);
+   vw_set_tx_pin(TRANSMITTER);
+   vw_setup(500);	 // Bits per sec
+   */
  }
 
-int count =0;
-volatile bool FLAG = false;
 void loop()
 {
-  ProcessSerial();
-  if(FLAG){
-    blink(2);
-    FLAG=false;
-   }
+  processSerial();
+
+  if(SENSORFLAG){
+    renderSensorData();
+    blink();
+    SENSORFLAG=false;
+  }
   
   if(stringComplete){
-    blink(1);
-    RemoteReceiver::disable();
-    boolean onOf = false;
-    char address = 0;
-    char device = 0;
-    char group = 0;
+   processStringComplete();
+   stringComplete = false;
+   blink();
+  }
+  
+  if (REMOTEFLAG){
+   for (char dev='A'; dev<='D'; dev++)
+    {
+      for(char b=0; b <= 1; b++)
+      {
+       unsigned long test = elroTransmitter.getTelegram(SYSTEMCODE, dev, b);     
+       if (RemoteTransmitter::isSameCode(test, lastCode))
+       {
+         Serial.print("r");
+         Serial.print(dev);
+         if(b == 0)
+           Serial.println("0");
+         else
+           Serial.println("1");
+         Serial.flush();
+       } 
+     }  
+    }
+    blink();
+    REMOTEFLAG=false;
+   }
+      
+    
+}
+
+void renderSensorData(){
+ if ( (data[3]) == 0xF0 ) {							
+      int channel=((data[1]&0x30)>>4)+1;
+      byte high=((data[1]&0x08)?0xf0:0)+(data[1]&0x0f);
+      byte low=data[2];
+      int temp10= high*256+low;
+      Serial.print("c:");
+      Serial.print(channel);
+    
+      Serial.flush();
+      Serial.print(";t");
+      //Serial.print(temp10/10);
+      //Serial.print(".");
+      Serial.print(temp10);
+      //Serial.print(abs((temp10)%10));
+      Serial.flush();
+      
+      Serial.print(";b");
+      Serial.println(data[0],DEC);
+      Serial.flush(); 
+ }
+}
+
+void interruptWrapper(){
+  //HACK to make print work proper
+  if(stringComplete || SENSORFLAG || REMOTEFLAG)
+    {
+      return;
+    }
+  //make rising/falling work alas "CHANGE"
+  uint8_t bit = digitalPinToBitMask(RECEIVER);
+  P1IES ^= bit;
+
+  SpaceLen::interruptHandler();
+  RemoteReceiver::interruptHandler();
+}
+
+void handleSensorData(byte *datain) {
+  SENSORFLAG = true;
+  for (int x=0; x<4;x++){
+    data[x] = datain[x];
+  }
+
+}
+
+void processSerial() {
+  while (Serial.available() > 0) {
+    char inChar = (char)Serial.read(); 
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    } 
+  }
+}
+
+void processStringComplete()
+{
+  interrupts();
+   boolean onOf = false;
+   char address = 0;
+   char device = 0;
+    
     
     switch(inputString[0]){
       case 'h':
-        p("usage:");
-        p("action:  a|0..31 (char)sys|A-E (charCap)dev|0-1(on/of)");
-        p("blokker: b|1-8 (char)|0-1 onof(char)");
-        p("Elro:    e|0..31(char)|A-D CAP||0-1(on/of");
-        p("KAKU:    k|A-P (char)add|1..4(char)group|1..4 (char)dev) |0-1 onof|");
-        p("KAKU:    k|A-P (char)add|1..4 (char)dev) |0-1 onof|");
-        p("Test:    t\\r");
-        p("");
-        p("eg: a31B1 -> put 31 as char 31 (one byte)");    
+        Serial.println("HELP NOT IMPLEMEMENTED YET");
+       // Serial.print(HELP);
+        Serial.flush();
+       
         break;
-        
+       case 'i':
+        Serial.println("i hit");
+       break;
+       
        case 'a':
          address = inputString[1];
          device = inputString[2];
@@ -113,77 +230,15 @@ void loop()
     p("ok");
      //Clear state of the input and re-enable receiver
     inputString = ""; 
-    RemoteReceiver::enable();
     stringComplete = false;
-  
-  }
 }
-
-void ProcessSerial() {
-  while (Serial.available() > 0) {
-    char inChar = (char)Serial.read(); 
-    inputString += inChar;
-    if (inChar == '\n') {
-      stringComplete = true;
-    } 
-  }
-}
-
   
 void translateCode(unsigned long receivedCode, unsigned int period){
-   interrupts();
-   FLAG=true;
-   RemoteReceiver::disable();
- /* 
-  Serial.print("Code: ");
-  Serial.print(receivedCode, HEX);
-  Serial.flush();
-  Serial.print(", period duration: ");
-  Serial.print(period);
-  Serial.println("us.");
-  */
- // brute(receivedCode);
- // RemoteTransmitter::sendCode(P1_4, receivedCode, period, 3);
-  for (char dev='A'; dev<='D'; dev++)
-  {
-    for(char b=0; b <= 1; b++)
-    {
-     unsigned long test = elroTransmitter.getTelegram(SYSTEMCODE, dev, b);     
-     if (RemoteTransmitter::isSameCode(test, receivedCode))
-     {
-       Serial.print("r");
-       Serial.print(dev);
-       if(b == 0)
-         Serial.println("0");
-       else
-         Serial.println("1");
-       Serial.flush();
-     } 
-   }  
-  }
-   
- RemoteReceiver::enable();
-}
+    REMOTEFLAG=true;
+    lastCode = receivedCode;
+    lastPeriod = period; 
+   }
 
-//BruteForce the possible command:
-void brute(long input){
-  for (char address=B00000; address<=B11111;address++){
-    for (int dev=1; dev<=4;dev++){
-      for (int b=0; b<=1;b++){
-        long elro = elroTransmitter.getTelegram(dev,address, b);
-   
-        if(RemoteTransmitter::isSameCode(elro, input))
-        {
-          Serial.println("found elro");
-        }
-   
-      }
-    }
-  }
-  
-    
-  Serial.println("no match found");
-}
 
 void blink(int times){
   for (int i=0; i< times; i++)
